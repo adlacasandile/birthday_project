4e55adb18423f1857149abd302bb9caac4f97a24
Taray <angelique.lacasandile@gmail.com>
2021-01-18 02:01:50 +0800

    initial code

---
+++ src/birthday/birth.java
@@ -0,0 +1,24 @@
+package birthday;
+import java.util.Scanner;
+
+public class birth {
+
+
+	public static void main(String[] args) {
+		
+		int result = recursion(2);
+		System.out.println(""+result);
+	}
+	
+	public static int recursion(int n){
+		if(n==0)
+		{
+			return 0;
+		}
+		else
+		{
+			return n + recursion(n-1);
+		}
+	}
+
+}
